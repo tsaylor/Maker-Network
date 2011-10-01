e88b7f93728753b928ea18b030c6a4a38b67df1a
Tim Saylor <tsaylor@psclistens.com>
2011-10-01 15:29:21 -0500

    added a signal to create a user profile on registration

--- general/models.py
+++ general/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 from taggit.managers import TaggableManager
+
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
@@ -14,5 +16,14 @@ class UserProfile(models.Model):
 
     def get_absolute_url(self):
         return reverse('general.views.view_profile')
+
+def create_user_profile(sender, **kwargs):
+    """ Creates a UserProfile for each new User """
+    if kwargs['created'] == True:
+        up = UserProfile(user=kwargs['instance'])
+        up.save()
+        
+post_save.connect(create_user_profile, sender=User)
+
 
 admin.site.register(UserProfile)
