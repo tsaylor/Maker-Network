6fe8a803dc02e5fa4c32874792badda619b18cc0
Tim Saylor <tsaylor@psclistens.com>
2011-10-02 02:09:07 -0500

    added joining and leaving groups

--- general/views.py
+++ general/views.py
@@ -1,9 +1,28 @@
 from django.views.generic.create_update import update_object
-from general.models import UserProfile
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+import general.models as models
 
 
 def view_profile(request):
     return edit_profile(request)
 
 def edit_profile(request):
-    return update_object(request, model=UserProfile, object_id=request.user.pk)
+    return update_object(request, model=models.UserProfile, object_id=request.user.pk)
+
+def leave_organization(request, object_id):
+    if request.method == "POST":
+        org = get_object_or_404(models.Organization, id = object_id)
+        org.members.remove(request.user)
+        messages.add_message(request, messages.INFO, 'You have left %s' % org.name)
+    return HttpResponseRedirect(reverse('organization_detail', kwargs={'object_id':org.id}))
+
+def join_organization(request, object_id):
+    if request.method == "POST":
+        org = get_object_or_404(models.Organization, id = object_id)
+        org.members.add(request.user)
+        messages.add_message(request, messages.INFO, 'You have joined %s' % org.name)
+    return HttpResponseRedirect(reverse('organization_detail', kwargs={'object_id':org.id}))
