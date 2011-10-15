95090897774a880b2b5c230797f24d835ad29425
eviljoel <eviljoel@linux.com>
2011-10-15 00:24:45 -0400

    Added skills list.

--- general/skill_urls.py
+++ general/skill_urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from general.models import Skill
 
 urlpatterns = patterns('',
-    #url(r'^$', 'django.views.generic.list_detail.object_list', name='organization_list', kwargs={'queryset':Organization.objects.all()}),
+    url(r'^$', 'django.views.generic.list_detail.object_list', name='skill_list', kwargs={'queryset':Skill.objects.all()}),
     url(r'^(?P<object_id>[0-9]+)/$', 'django.views.generic.list_detail.object_detail', name='skill_detail', kwargs={'queryset':Skill.objects.all(), 'template_object_name':'skill'}),
 )
